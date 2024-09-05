1499d42add455b1386341f4fe7675aa034b29be8
Lakshmi Narayanan PV <Lakshmi.Narayanan1@ibm.com>
2024-09-05 18:32:08 +0530

    Added the copyright text and fixed the linter issues

--- db2whmigratetocos/constants.py
+++ db2whmigratetocos/constants.py
@@ -1,2 +1,10 @@
+
+"""
+
+    Copyright IBM Corp. 2024-2025 All Rights Reserved.
+    Licensed Materials - Property of IBM
+
+"""
+
 VERSION_NAME = "0.0.1"
-PACKAGE_NAME = "db2whmigratetocos-"+VERSION_NAME+"-py3-none-any.whl" 
+PACKAGE_NAME = "db2whmigratetocos-"+VERSION_NAME+"-py3-none-any.whl"
