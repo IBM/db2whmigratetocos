dce31f12a8336050a948313754e80ab9cfad1153
Ruhi Sehgal <rusehga1@in.ibm.com>
2024-10-01 11:39:50 +0530

    unit test cases for listing TS,SCHEMA and tables under TS

--- build_pytest.sh
+++ build_pytest.sh
@@ -9,6 +9,10 @@ pwd
 cd ../tests
 ssh -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev root@9.30.214.235 "cd /root/db2migrate/db2whmigratetocos-0.0.1 && pwd"
 scp -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev  content.json  root@9.30.214.235:/root/db2migrate/db2whmigratetocos-0.0.1/tests
-scp -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev  content_copy.json  root@9.30.214.235:/root/db2migrate/db2whmigratetocos-0.0.1/tests
+#scp -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev  content_copy.json  root@9.30.214.235:/root/db2migrate/db2whmigratetocos-0.0.1/tests
 #ssh -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev root@9.30.214.235 "cd /root/db2migrate/db2whmigratetocos-0.0.1  && python -m unittest tests/test_connection_string.py"
-ssh -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev root@9.30.214.235 "cd /root/db2migrate/db2whmigratetocos-0.0.1  && python -m unittest tests/test_schema_in_instance.py"
+#ssh -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev root@9.30.214.235 "cd /root/db2migrate/db2whmigratetocos-0.0.1  && python -m unittest tests/test_schema_in_instance.py"
+#ssh -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev root@9.30.214.235 "cd /root/db2migrate/db2whmigratetocos-0.0.1  && python -m unittest tests/test_tablespaces_in_block_and_cos.py"
+#ssh -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev root@9.30.214.235 "cd /root/db2migrate/db2whmigratetocos-0.0.1  && python -m unittest tests/test_tables_under_tablespace_in_db2woc.py"
+#ssh -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev root@9.30.214.235 "cd /root/db2migrate/db2whmigratetocos-0.0.1  && python -m unittest tests/test_tablespace_name_for_table.py"
+ssh -i /Users/ruhisehgal/.ssh/db2whmigratetocos-dev root@9.30.214.235 "cd /root/db2migrate/db2whmigratetocos-0.0.1  && python -m unittest tests/test_tables_under_schema_in_db2woc.py"
